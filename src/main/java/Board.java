409d645f34b2a0b78b40c402f5269cd17f83fbf3
tbrkcbn <tugberkcoban@gmail.com>
2021-05-01 01:33:05 +0300

    messed backup :(

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -12,32 +12,43 @@ public class Board {
     public Board(int boardLength, int boardHeigth) {
         this.boardLength = boardLength;
         this.boardHeigth = boardHeigth;
-        this.board = new char[this.boardHeigth][this.boardLength];
-        for (int i = 0;i < this.boardHeigth;i++ ){
-            for (int j = 0;j < this.boardLength;j++ ){
+        this.board = new char[this.boardLength][this.boardHeigth];
+        for (int i = 0; i < this.boardLength; i++ )
+            for (int j = 0; j < this.boardHeigth; j++ )
                 board[i][j] = water;
-            }
-        }
     }
 
-    public void showBoard(){
+    public void showGameBoard(){
         System.out.println("        A B C D E F \n        -----------");
-        for (int i = 0;i < this.boardHeigth;i++ ){
-            if(i == (boardHeigth/2)-1){
+        for (int i = 0;i < this.boardLength;i++ ){
+            if(i == (boardLength/2)-1){
                 System.out.print("    "+(i+1)+" | ");
-                for (int j = 0;j < this.boardLength;j++ ){
+                for (int j = 0;j < this.boardHeigth;j++ ){
                     System.out.print(board[i][j]+" ");
                 }
                 System.out.print("\n");
                 System.out.println("      | -----------");
             }else{
                 System.out.print("    "+(i+1)+" | ");
-                for (int j = 0;j < this.boardLength;j++ ){
+                for (int j = 0;j < this.boardHeigth;j++ ){
                     System.out.print(board[i][j]+" ");
                 }
                 System.out.print("\n");
             }
         }
+    }
+    public void showPersonalBoard(){
+        System.out.println("        A B C D E F \n        -----------");
+        for (int i = 0;i < this.boardLength;i++ ){
+                System.out.print("    "+(i+1)+" | ");
+                for (int j = 0;j < this.boardHeigth;j++ ){
+                    System.out.print(board[i][j]+" ");
+                }
+                System.out.print("\n");
+        }
+
+
+
     }
 
     public void hit(int coordinatesX, int coordinatesY){
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -8,14 +9,19 @@ public class Main {
         int choice = 0;
         int boardLength = 6;
         int boardHeigth = 8;
-        Board board = randomBoardGenerator(boardLength,boardHeigth);
-        board.showBoard();
-
 
         while (choice != -1) {
             choice = menu(opponentName, difficulty);
             switch (choice) {
                 case 1:
+                    Board opponentBoard = randomBoardGenerator(boardLength,boardHeigth/2,3);
+
+                    Board userBoard = userBoardPlacement(boardLength, boardHeigth/2,3);
+
+                    Board gameBoard =  joinUserOpponentBoard(opponentBoard,userBoard);
+                    gameBoard.showGameBoard();
+                    gameBoard.showPersonalBoard();
+
                     switch (difficulty) {
                         case "HARD":
                             hardGame(opponentName);
@@ -119,12 +125,198 @@ public class Main {
     public static void easyGame(String opponentName) {
     }
 
-    public static Board randomBoardGenerator(int boardLength, int boardHeigth) {
-        Board board = new Board(boardLength, boardHeigth);
+    public static Board randomBoardGenerator(int boardLength, int boardHeigth, int shipNumber) {
+        Board myBoard = new Board(boardLength, boardHeigth);
+        Random rand = new Random();
+        int placedShips = 0;
+        int x,y;
+        int direction;
 
+        while(placedShips < shipNumber){
 
-//        board.showBoard();
+            x = rand.nextInt(boardLength);
+            y = rand.nextInt(boardHeigth);
 
-        return board;
+            if(myBoard.board[x][y] == 'O'){
+
+                direction = rand.nextInt(3);
+                switch (direction){
+                    case 0:
+                        //right
+                        if(x+1 < boardLength){
+                            if (myBoard.board[x+1][y] == 'O'){
+                                myBoard.board[x][y] = 'S';
+                                myBoard.board[x+1][y] = 'S';
+                                placedShips++;
+                            }
+                        }
+                        break;
+                    case 1:
+                        //up
+                        if(y+1 < boardHeigth){
+                            if (myBoard.board[x][y+1] == 'O'){
+                                myBoard.board[x][y] = 'S';
+                                myBoard.board[x][y+1] = 'S';
+                                placedShips++;
+                            }
+                        }
+                        break;
+                    case 2:
+                        //left
+                        if(x-1 >= 0){
+                            if (myBoard.board[x-1][y] == 'O'){
+                                myBoard.board[x][y] = 'S';
+                                myBoard.board[x-1][y] = 'S';
+                                placedShips++;
+                            }
+                        }
+                        break;
+                    default:
+                        //down
+                        if(y-1 >= 0){
+                            if (myBoard.board[x][y-1] == 'O'){
+                                myBoard.board[x][y] = 'S';
+                                myBoard.board[x][y-1] = 'S';
+                                placedShips++;
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+        return myBoard;
+    }
+
+    public static Board userBoardPlacement(int boardLength, int boardHeigth, int shipNumber){
+        Board userBoard = new Board(boardLength,boardHeigth);
+        Scanner scanner = new Scanner(System.in);
+        int placedShips = 0;
+        boolean letterFlag = true;
+        boolean numberFlag = true;
+        String choice;
+        int x = 0,y = 0;
+        int direction;
+
+        while(placedShips < shipNumber){
+            userBoard.showPersonalBoard();
+            System.out.println("\nPlease give the coordinates for start point of your ship number "+(placedShips+1)+" : ");
+            choice = scanner.nextLine();
+            if(choice.length() == 2){
+                switch (choice.toLowerCase().charAt(0)){
+                    case 'a':
+                        x = 0;
+                        letterFlag = true;
+                        break;
+                    case 'b':
+                        x = 1;
+                        letterFlag = true;
+                        break;
+                    case 'c':
+                        x = 2;
+                        letterFlag = true;
+                        break;
+                    case 'd':
+                        x = 3;
+                        letterFlag = true;
+                        break;
+                    case 'e':
+                        x = 4;
+                        letterFlag = true;
+                        break;
+                    case 'f':
+                        x = 5;
+                        letterFlag = true;
+                        break;
+                    default:
+                        System.out.println("Wrong letter coordinate given, try again");
+                        letterFlag = false;
+                }
+                y = Integer.parseInt(String.valueOf(choice.charAt(1)));
+                y--;
+                if (y < boardHeigth && y >= 0)
+                    numberFlag = true;
+                else
+                    numberFlag = false;
+
+                if(letterFlag && numberFlag){
+                    if (userBoard.board[x][y] == 'O'){
+                        System.out.println("Which direction do you want to place this ship?");
+                        System.out.println("1. Right");
+                        System.out.println("2. Up");
+                        System.out.println("3. Left");
+                        System.out.println("4. Down");
+
+                        direction = Integer.parseInt(String.valueOf(scanner.nextLine()));
+                        if(direction < 4 && direction >= 0){
+                            switch (direction){
+                                case 1:
+                                    //right
+                                    if(x+1 < boardLength){
+                                        if (userBoard.board[x+1][y] == 'O'){
+                                            userBoard.board[x][y] = 'S';
+                                            userBoard.board[x+1][y] = 'S';
+                                            placedShips++;
+                                        }else
+                                            System.out.println("Invalid direction!");
+                                    }
+                                    break;
+                                case 2:
+                                    //up
+                                    if(y+1 < boardHeigth){
+                                        if (userBoard.board[x][y+1] == 'O'){
+                                            userBoard.board[x][y] = 'S';
+                                            userBoard.board[x][y+1] = 'S';
+                                            placedShips++;
+                                        }else
+                                            System.out.println("Invalid direction!");
+                                    }
+                                    break;
+                                case 3:
+                                    //left
+                                    if(x-1 >= 0){
+                                        if (userBoard.board[x-1][y] == 'O'){
+                                            userBoard.board[x][y] = 'S';
+                                            userBoard.board[x-1][y] = 'S';
+                                            placedShips++;
+                                        }else
+                                            System.out.println("Invalid direction!");
+                                    }
+                                    break;
+                                default:
+                                    //down
+                                    if(y-1 >= 0){
+                                        if (userBoard.board[x][y-1] == 'O'){
+                                            userBoard.board[x][y] = 'S';
+                                            userBoard.board[x][y-1] = 'S';
+                                            placedShips++;
+                                        }else
+                                            System.out.println("Invalid direction!");
+                                    }
+                                    break;
+                            }
+                        }else
+                            System.out.println("İnvalid Choice!");
+                    }else
+                        System.out.println("Given start coordination is invalid, try again");
+                }else
+                    System.out.println("Invalid coordinates!");
+            }else
+                System.out.println("Choice must be a valid coordinate (ex. a3)");
+        }
+        return userBoard;
+    }
+
+    public static Board joinUserOpponentBoard(Board opponentBoard, Board userBoard){
+        int boardHeigth = opponentBoard.boardHeigth*2;
+        Board gameBoard = new Board(opponentBoard.boardLength,boardHeigth);
+
+        for(int x = 0; x < gameBoard.boardLength; x++)
+            for (int y = 0; y < gameBoard.boardHeigth; y++)
+                if(y < opponentBoard.boardHeigth)
+                    gameBoard.board[x][y] = opponentBoard.board[x][y];
+                else
+                    gameBoard.board[x+ opponentBoard.boardHeigth][y+opponentBoard.boardHeigth] = userBoard.board[x][y];
+
+        return gameBoard;
     }
 }
